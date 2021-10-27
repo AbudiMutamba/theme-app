6b7d08b77a1b0bec6bfaec56bf5ecba6780c0b43
AbudiMutamba <mutambaabudi@gmail.com>
2021-10-27 17:11:24 +0300

    House cleaning

--- src/components/routes/App.js
+++ src/components/routes/App.js
@@ -1,30 +1,31 @@
 import * as React from 'react'
 // import {Link} from 'react-router-dom'
 
-import Home from '../views/Home' 
-
-import {
-    BrowserRouter as Router,
-    Switch, Route, Link
-} from 'react-router-dom'
 
 function App() {
-    //const [page, setPage] = React.useState(null)
+    const [theme, switchTheme ] = React.useState('')
+
+    const handleClick = (switchTo) => {
+        switchTheme(switchTo)
+        console.log(theme)
+    }
 
     return (
-        <Router>
-        
-            <Switch>
-                <Route path="/" exact>
-                    <Home />
-                </Route>
-                
+        <div className="base black">
+            <h1>The theme is <SayTheme theme={theme} /></h1>
 
+            <input type="button" className="btn black" onclick={() => handleClick('.black')} />
 
-            </Switch>
+            <input type="button" className="btn blue" onclick={() => handleClick('.blue')}/>
 
-        </Router>
+            <input type="button" className="btn yellow" onclick={() => handleClick('.yellow')} />
+        </div>
     )
 }
 
 export default App
+
+
+function SayTheme (props) {
+    return <span>{props.themeName}</span>
+}
